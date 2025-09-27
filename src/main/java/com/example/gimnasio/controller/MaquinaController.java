579469f3f1bdc135dd5d7d65efb2201ab8713e61
Chrysr17 <chrisrasaro@gmail.com>
2025-09-26 22:18:34 -0500

    commit6

--- src/main/java/com/example/gimnasio/controller/MaquinaController.java
+++ src/main/java/com/example/gimnasio/controller/MaquinaController.java
@@ -1,9 +1,12 @@
 package com.example.gimnasio.controller;
 
+import com.example.gimnasio.entity.Maquina;
 import com.example.gimnasio.service.MaquinaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/maquinas")
@@ -13,6 +16,10 @@ public class MaquinaController {
 
     public MaquinaController(MaquinaService maquinaService) {
         this.maquinaService = maquinaService;
+    }
 
+    @GetMapping
+    public List<Maquina> listar(){
+        return maquinaService.listarTodas();
     }
 }
