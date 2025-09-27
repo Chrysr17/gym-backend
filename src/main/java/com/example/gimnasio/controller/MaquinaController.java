3708b721959aa77e91d3653b410b712ed96470ac
Chrysr17 <chrisrasaro@gmail.com>
2025-09-26 22:24:05 -0500

    commit7

--- src/main/java/com/example/gimnasio/controller/MaquinaController.java
+++ src/main/java/com/example/gimnasio/controller/MaquinaController.java
@@ -2,7 +2,9 @@ package com.example.gimnasio.controller;
 
 import com.example.gimnasio.entity.Maquina;
 import com.example.gimnasio.service.MaquinaService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +24,12 @@ public class MaquinaController {
     public List<Maquina> listar(){
         return maquinaService.listarTodas();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Maquina> buscarPorId(@PathVariable Integer Id){
+        return maquinaService.buscarPorId(Id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
