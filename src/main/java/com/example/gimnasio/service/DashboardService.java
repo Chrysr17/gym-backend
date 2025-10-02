4dacf2862d631083247f4fed0177ee1c9bd98161
Chrysr17 <chrisrasaro@gmail.com>
2025-10-01 20:51:51 -0500

    dashboardService

--- src/main/java/com/example/gimnasio/service/DashboardService.java
+++ src/main/java/com/example/gimnasio/service/DashboardService.java
@@ -5,4 +5,9 @@ import java.util.List;
 public interface DashboardService {
     List<Object[]> obtenerSedesConMasGanancias();
     List<Object[]> obtenerSedesMasConcurridas();
+
+    Double obtenerIngresosTotales();
+    Long obtenerTotalClientes();
+    Long obtenerTotalProveedores();
+    Long obtenerTotalMaquinas();
 }
