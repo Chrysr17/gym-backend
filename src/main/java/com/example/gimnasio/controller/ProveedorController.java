79167b0d3cd3d0621f4a60aaf91020165a54404d
Chrysr17 <chrisrasaro@gmail.com>
2025-10-03 18:37:51 -0500

    ProveedorController

--- src/main/java/com/example/gimnasio/controller/ProveedorController.java
+++ src/main/java/com/example/gimnasio/controller/ProveedorController.java
@@ -1,4 +1,55 @@
 package com.example.gimnasio.controller;
 
+import com.example.gimnasio.entity.Proveedor;
+import com.example.gimnasio.service.ProveedorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/proveedores")
 public class ProveedorController {
+
+    private final ProveedorService proveedorService;
+
+    public ProveedorController(ProveedorService proveedorService) {
+        this.proveedorService = proveedorService;
+    }
+
+    @GetMapping
+    public List<Proveedor> listar(){
+        return proveedorService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Proveedor> buscarPorId(@PathVariable Integer id){
+        return proveedorService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Proveedor> guardar(@RequestBody Proveedor proveedor){
+        Proveedor nuevo = proveedorService.guardar(proveedor);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Proveedor> actualizar(@PathVariable Integer id, @RequestBody Proveedor proveedor){
+        try {
+            Proveedor actualizado =  proveedorService.actualizar(id, proveedor);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Integer id){
+        proveedorService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
