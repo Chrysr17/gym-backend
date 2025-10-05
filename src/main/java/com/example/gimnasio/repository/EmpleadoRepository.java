2016a1ad79af8b95c0ce7fd3827a38bbefe495c1
Chrysr17 <chrisrasaro@gmail.com>
2025-10-04 20:23:23 -0500

    empleadoRepository 2

--- src/main/java/com/example/gimnasio/repository/EmpleadoRepository.java
+++ src/main/java/com/example/gimnasio/repository/EmpleadoRepository.java
@@ -1,9 +1,14 @@
 package com.example.gimnasio.repository;
 
+import com.example.gimnasio.entity.Cargo;
 import com.example.gimnasio.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+    List<Empleado> findBySede_SedeId(Integer sedeId);
+    List<Empleado> findByCargp(Cargo cargo);
 }
