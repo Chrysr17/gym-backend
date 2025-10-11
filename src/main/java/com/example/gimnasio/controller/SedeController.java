7f9eede181bbdad98d55decefcd0ad23cd38c5e1
Chrysr17 <chrisrasaro@gmail.com>
2025-10-11 17:57:14 -0500

    SedeController - Operation(summary)

--- src/main/java/com/example/gimnasio/controller/SedeController.java
+++ src/main/java/com/example/gimnasio/controller/SedeController.java
@@ -2,6 +2,8 @@ package com.example.gimnasio.controller;
 
 import com.example.gimnasio.entity.Sede;
 import com.example.gimnasio.service.SedeService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +21,13 @@ public class SedeController {
     }
 
     @GetMapping
+    @Operation(summary = "Lista todas las sedes")
     public List<Sede> listar(){
         return sedeService.listarTodas();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Busca una sede por Id")
     public ResponseEntity<Sede> buscarPorId(@PathVariable Integer id){
         return sedeService.buscarPorId(id)
                 .map(ResponseEntity::ok)
@@ -31,13 +35,15 @@ public class SedeController {
     }
 
     @PostMapping
+    @Operation(summary = "Guarda una sede")
     public ResponseEntity<Sede> guardar(@RequestBody Sede sede){
         Sede nueva = sedeService.guardar(sede);
         return ResponseEntity.status(HttpStatus.CREATED).body(nueva);
     }
 
     @PutMapping
-    public ResponseEntity<Sede> actualziar(@PathVariable Integer id, @RequestBody Sede sede){
+    @Operation(summary = "Actualiza una sede por id")
+    public ResponseEntity<Sede> actualizar(@PathVariable Integer id, @RequestBody Sede sede){
         try {
             Sede actualizada = sedeService.actualizarSede(id, sede);
             return ResponseEntity.ok(actualizada);
@@ -47,6 +53,7 @@ public class SedeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina una sede por id")
     public ResponseEntity<Void> eliminar(@PathVariable Integer id){
         sedeService.buscarPorId(id);
         return ResponseEntity.noContent().build();
