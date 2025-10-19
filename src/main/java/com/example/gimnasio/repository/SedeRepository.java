1f1e4681973b98f2eebc1f7cbb5afd127cfe5b5d
Chrysr17 <chrisrasaro@gmail.com>
2025-10-19 15:10:07 -0500

    SedeRepository - findByNombre

--- src/main/java/com/example/gimnasio/repository/SedeRepository.java
+++ src/main/java/com/example/gimnasio/repository/SedeRepository.java
@@ -4,6 +4,9 @@ import com.example.gimnasio.entity.Sede;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SedeRepository extends JpaRepository<Sede, Integer> {
+    List<Sede> findByNombreContainigIgnoreCase(String nombre);
 }
