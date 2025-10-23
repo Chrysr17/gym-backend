bcf8291ec5f8ff06f0f713b92a03f341fba4a96b
Chrysr17 <chrisrasaro@gmail.com>
2025-10-23 17:26:30 -0500

    PagoRepository - findByfecha

--- src/main/java/com/example/gimnasio/repository/PagoRepository.java
+++ src/main/java/com/example/gimnasio/repository/PagoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,8 @@ import java.util.Optional;
 public interface PagoRepository extends JpaRepository<Pago, Integer> {
 
     List<Pago> findByClienteClienteId(Integer clienteId);
+
+    List<Pago> findByFechaBetween(LocalDate inicio, LocalDate fin);
 
     @Query("SELECT SUM(p.monto) FROM Pago p WHERE p.estado = 'Pagado'")
     Optional <Double> sumPagosCompletados();
