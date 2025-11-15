8bcdb1f7b89805a392c790c4f218819a6191dcb1
Chrysr17 <chrisrasaro@gmail.com>
2025-11-14 19:53:51 -0500

    UsuarioController

--- src/main/java/com/example/gimnasio/controller/AuthController.java
+++ src/main/java/com/example/gimnasio/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.gimnasio.entity.Rol;
 import com.example.gimnasio.entity.Usuario;
 import com.example.gimnasio.repository.UsuarioRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +18,11 @@ import java.util.Optional;
 public class AuthController {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(UsuarioRepository usuarioRepository) {
+    public AuthController(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
@@ -27,25 +30,23 @@ public class AuthController {
                                    @RequestParam String password) {
         Optional<Usuario> usuarioOptional = usuarioRepository.findByNombreUsuario(nombreUsuario);
 
-        if (usuarioOptional.isPresent()) {
-            Usuario usuario = usuarioOptional.get();
-
-            if (usuario.getPassword().equals(password)) {
-                if (usuario.getRol() == Rol.CLIENTE) {
-                    Cliente cliente = usuario.getCliente();
-                    if (cliente != null) {
-                        return ResponseEntity.ok(cliente);
-                    } else {
-                        return ResponseEntity.badRequest().body("Cliente no asociado al usuario");
-                    }
-                } else {
-                    return ResponseEntity.ok(usuario);
-                }
-            } else {
-                return ResponseEntity.badRequest().body("Contraseña incorrecta");
-            }
-        } else {
+        if (usuarioOptional.isEmpty()) {
             return ResponseEntity.badRequest().body("Usuario no encontrado");
         }
+
+        Usuario usuario = usuarioOptional.get();
+        if (!passwordEncoder.matches(password, usuario.getPassword())) {
+            return ResponseEntity.badRequest().body("Contraseña incorrecta");
+        }
+
+        if (usuario.getRol() == Rol.CLIENTE) {
+            Cliente cliente = usuario.getCliente();
+            if (cliente != null) {
+                return ResponseEntity.ok(cliente);
+            } else {
+                return ResponseEntity.badRequest().body("Cliente no asociado al usuario");
+            }
+        }
+        return ResponseEntity.ok(usuario);
     }
 }
